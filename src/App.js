9e679f855a702e2b4ab684062920fac65555c087
theseanco <theseancotterill@live.com>
2019-08-03 16:28:01 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -9,37 +9,34 @@ const AppContainer = styled.div`
   position: relative;
 `;
 
+// Generate a unique (enough) ID (without overkill external deps)
 const generateID = () => Math.random().toString(16).slice(-6);
 
-// test array
-const idArray = Array.from({ length: 10 }, () => generateID());
-
-const newIDArray = [generateID()];
-
-// setInterval(() => { newIDArray.push(generateID()); }, 4000);
+// Initial array of dogs for starting the app
+const idArray = Array.from({ length: 5 }, () => generateID());
 
 function App() {
-  const [dogInterval, setDogInterval] = useState();
+  // Interval to add dogs to end of array
+  const [dogAddingInterval, setDogAddingInterval] = useState();
+  // Interval to remove dogs from start of array
+  const [dogRemovingInterval, setDogRemovingInterval] = useState();
+  // Array of dogs
   const [dogArray, setDogArray] = useState(idArray);
 
-  // This makes the code work, but i don't really know why.
+  // This starts off the interval which adds new dogs every second
   useEffect(() => {
-    /*
-    setDogInterval(setInterval(() => setDogArray((prevDogArray) => {
-      const newArray = prevDogArray;
-      newArray.push(generateID());
-      return newArray;
-    }), 5000));
-    */
-    setDogInterval(setInterval(() => setDogArray(prevDogArray => prevDogArray.concat([generateID()])), 5000));
+    setDogAddingInterval(
+      setInterval(() => setDogArray(
+        prevDogArray => prevDogArray.concat([generateID()]),
+      ),
+      1000),
+    );
   }, []);
-
-  console.log(dogArray);
 
   return (
     <AppContainer>
       {
-        dogArray.map((uniqueID, index) => (
+        dogArray.map(uniqueID => (
           <DogDisplay key={uniqueID} />
         ))
       }
